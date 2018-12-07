6083f657be3e64ada8ab8bd1f8aeff4b36f8e7aa
goodkaiman <32721809+goodkaiman@users.noreply.github.com>
2018-12-06 16:00:07 -0800

    Update js/grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -64,7 +64,7 @@ var TxtType = function(el, toRotate, period) {
         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
         var that = this;
-        var delta = 140 - Math.random() * 100;
+        var delta = 100 - Math.random() * 100;
 
         if (this.isDeleting) { delta /= 2; }
 
